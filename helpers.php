075e27574bea93a56e3a309854c22f12137cf453
isabella-projects <damyan4oy@gmail.com>
2024-01-26 16:35:42 +0100

    Added 2 more helper functions

--- helpers.php
+++ helpers.php
@@ -18,7 +18,6 @@ function basePath($path = '')
  */
 function loadView($name)
 {
-
     $viewPath = basePath("views/{$name}.view.php");
 
     if (file_exists($viewPath)) {
@@ -42,4 +41,30 @@ function loadPartial($name)
     } else {
         echo "View '{$name} not found!'";
     }
+}
+
+/**
+ * Inspect a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
 }
