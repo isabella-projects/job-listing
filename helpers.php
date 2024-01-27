94cf6d3087f6e37ef0d52493ab4cfd43e97abaf4
isabella-projects <damyan4oy@gmail.com>
2024-01-27 18:15:12 +0100

    Query methods, fetching, views and finishing database functionalities

--- helpers.php
+++ helpers.php
@@ -16,11 +16,12 @@ function basePath($path = '')
  * @param string $name
  * @return void
  */
-function loadView($name)
+function loadView($name, $data = [])
 {
     $viewPath = basePath("views/{$name}.view.php");
 
     if (file_exists($viewPath)) {
+        extract($data);
         require $viewPath;
     } else {
         echo "View '{$name} not found!'";
@@ -67,4 +68,42 @@ function inspectAndDie($value)
     echo '<pre>';
     die(var_dump($value));
     echo '</pre>';
+}
+
+/**
+ * Format the tags with uppercase
+ *
+ * @param mixed $tags
+ * @return string
+ */
+function formatTags($tags)
+{
+    $tagArray = explode(',', $tags);
+    $capitalizedTags = array_map('ucwords', $tagArray);
+    return implode(', ', $capitalizedTags);
+}
+
+/**
+ * Format salary with currency
+ *
+ * @param string $salary
+ * @param string $currency
+ * @return void
+ */
+function formatSalary($salary, $currency = 'EUR')
+{
+    $formattedSalary = number_format($salary, 0, '.', ',');
+
+    $currencySymbols = [
+        'EUR' => '€',
+        'USD' => '$',
+        'GBP' => '£',
+        'BGR' => 'лв.',
+    ];
+
+    if ($currency === 'BGR') {
+        return $formattedSalary . ' ' . $currencySymbols[$currency];
+    } else {
+        return $currencySymbols[$currency] . '' . $formattedSalary;
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,18 +1,18 @@
 <?php
 require '../helpers.php';
-require basePath('Database.php');
-
-$config = require(basePath('config/db.php'));
-
-$db = new Database($config);
 
 require basePath('Router.php');
+require basePath('Database.php');
 
+// Instatiating the router
 $router = new Router();
 
+// Getting routes
 $routes = require(basePath('routes.php'));
 
-$uri = $_SERVER['REQUEST_URI'];
+// Get current URI and HTTP method
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Route the request
 $router->route($uri, $method);
