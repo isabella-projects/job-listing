425201a9d4d8c3d0c64e5c3ccea9aa0942368ffb
isabella-projects <damyan4oy@gmail.com>
2024-01-28 15:16:01 +0100

    Installing composer and using namespaces

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
+
+require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
 
-require basePath('Framework/Router.php');
-require basePath('Framework/Database.php');
+use Framework\Router;
+
+// spl_autoload_register(function ($class) {
+//     $path = basePath('Framework/' . $class . '.php');
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+// });
 
 // Instatiating the router
 $router = new Router();
