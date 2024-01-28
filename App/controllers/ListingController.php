bcf44423d67dfe3b3d1472d1a1273e7b82d55e32
isabella-projects <damyan4oy@gmail.com>
2024-01-28 20:28:42 +0100

    Form submission and data sanitizing

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -63,4 +63,23 @@ class ListingController
             'listing' => $listing
         ]);
     }
+
+    /**
+     * Store data in database
+     * 
+     * @return void
+     */
+    public function store()
+    {
+        $allowedFields = [
+            'title', 'description', 'salary', 'tags',
+            'company', 'address', 'city', 'state',
+            'phone', 'email', 'requirements', 'benefits'
+        ];
+
+        $newListingData = array_intersect_key($_POST, array_flip($allowedFields));
+        $newListingData['user_id'] = 1;
+
+        $newListingData = array_map('sanitize', $newListingData);
+    }
 }
--- helpers.php
+++ helpers.php
@@ -107,3 +107,14 @@ function formatSalary($salary, $currency = 'EUR')
         return $currencySymbols[$currency] . '' . $formattedSalary;
     }
 }
+
+/**
+ * Sanitize data
+ * 
+ * @param string $dirty
+ * @return string
+ */
+function sanitize($dirty)
+{
+    return filter_var(trim($dirty), FILTER_SANITIZE_SPECIAL_CHARS);
+}
