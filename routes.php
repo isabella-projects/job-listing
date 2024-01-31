8637481e8d258c8976ec87bd875e966847979a75
isabella-projects <damyan4oy@gmail.com>
2024-01-31 12:35:57 +0100

    User controller and views

--- routes.php
+++ routes.php
@@ -1,9 +1,17 @@
 <?php
+/* Index route */
 $router->get('/', 'HomeController@index');
+/* ----------------------------------------- */
+/* Listings routes */
 $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create');
+$router->get('/listings/edit/{id}', 'ListingController@edit');
 $router->get('/listings/{id}', 'ListingController@show');
-
 $router->post('/listings', 'ListingController@store');
-
+$router->put('/listings/{id}', 'ListingController@update');
 $router->delete('/listings/{id}', 'ListingController@destroy');
+/* ------------------------------------------------------------ */
+/* Authentication routes */
+$router->get('/auth/register', 'UserController@create');
+$router->get('/auth/login', 'UserController@login');
+/* ------------------------------------------------ */
